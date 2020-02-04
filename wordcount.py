2125307107a1a4362d390602f6aa03579b5dc898
Your Name <you@example.com>
2020-02-04 09:41:39 -0800

    finished word count

--- wordcount.py
+++ wordcount.py
@@ -1,41 +1,34 @@
 # put your code here.
-input_file = open("test.txt")
-
-all_sentences = []
-for line in input_file:
-    sentence = line.split(" ")
-    sentence[-1] = sentence[-1][:-1]
-    all_words = all_words + sentence
-
-print(all_words)
-
-word_dict = {}
-
-for word in all_words:
-    if word not in dictionary:
-        word_dict[word] = 1
+# input_file = open("test.txt")
 
 
-    # word_dict.get()
+def count_words(input_file):
+    '''Print a dictionary with list of words and word count.'''
+    input_file = open(input_file)
+    all_words = []
+    for line in input_file:
+        sentence = line.lower()
+        sentence = sentence.split(" ")
+        sentence[-1] = sentence[-1][:-1]
+        all_words = all_words + sentence
+
+    word_dict = {}
+
+    for word in all_words:
+        if word not in word_dict:
+            word_dict[word] = 1
+        elif word in word_dict:
+            word_dict[word] += 1
+
+
+    for word, count in word_dict.items():
+        print(f'{word} {count}')
+
+
+# word_dict.get()
 
 
 # if word is not in dictionary, add word to dictionary with value of one
 # if word is in dictionary, incrememnt value by one
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # close("test.txt")
